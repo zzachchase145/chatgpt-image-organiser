f42c60f3acb318dac69edcffa756ef96b1204e87
Zach Chase <zzachchase@gmail.com>
2026-05-04 23:11:35 +0100

    Fixed browser script

--- browser_script.js
+++ browser_script.js
@@ -3,14 +3,15 @@ const images = [...document.querySelectorAll('img')]
   .map(img => img.src)
   .filter(src => src.includes('/backend-api/estuary/content'));
 
+console.log(images);
 console.log(`Found ${images.length} ChatGPT uploaded images`);
 
 // Download them
 images.forEach((url, index) => {
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = `chatgpt_image_${index + 1}.png`;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `image_${index}.png`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 });
